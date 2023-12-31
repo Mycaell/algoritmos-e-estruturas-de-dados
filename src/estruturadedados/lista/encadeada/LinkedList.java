3153ef737c2e83b5e7822dfb27383736e8dba887
Mycaell Oliveira <oliveiramycaell@gmail.com>
2023-12-31 17:26:01 -0300

    Create basic structure of LinkedList class

---
+++ src/estruturadedados/lista/encadeada/LinkedList.java
@@ -0,0 +1,11 @@
+package estruturadedados.lista.encadeada;
+
+public class LinkedList<T> {
+	
+	private Node<T> first;
+	private Node<T> last;
+	private int size = 0;
+
+	// TODO: create manipulation methods and toString
+	
+}
