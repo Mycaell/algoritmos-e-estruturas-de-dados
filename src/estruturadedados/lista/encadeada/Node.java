525cb025eba80cde61d7ff999cdab46db27d6cbc
Mycaell Oliveira <oliveiramycaell@gmail.com>
2023-12-31 17:24:24 -0300

    Create Node class

---
+++ src/estruturadedados/lista/encadeada/Node.java
@@ -0,0 +1,39 @@
+package estruturadedados.lista.encadeada;
+
+public class Node<T> {
+
+	private T item;
+	private Node<T> next;
+		
+	public Node(T item) {
+		this.item = item;
+		this.next = null;
+	}
+
+	public Node(T item, Node<T> next) {
+		this.item = item;
+		this.next = next;
+	}
+
+	public T getItem() {
+		return item;
+	}
+	
+	public void setItem(T item) {
+		this.item = item;
+	}
+	
+	public Node<T> getNext() {
+		return next;
+	}
+	
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [item=" + item + ", next=" + next + "]";
+	}
+	
+}
