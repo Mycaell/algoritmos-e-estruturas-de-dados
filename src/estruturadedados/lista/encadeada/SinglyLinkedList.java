eb2ca79d0f90c992f791abf6b95cbc4f57c317fb
Mycaell Oliveira <oliveiramycaell@gmail.com>
2024-01-01 21:57:56 -0300

    Change class name and implement size method

--- src/estruturadedados/lista/encadeada/SinglyLinkedList.java
+++ src/estruturadedados/lista/encadeada/SinglyLinkedList.java
@@ -1,11 +1,15 @@
 package estruturadedados.lista.encadeada;
 
-public class LinkedList<T> {
+public class SinglyLinkedList<T> {
 	
 	private Node<T> first;
 	private Node<T> last;
 	private int size = 0;
 
 	// TODO: create manipulation methods and toString
+	
+    public int size() {
+        return this.size;
+    }
 	
 }
